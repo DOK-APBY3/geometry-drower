6e04bfb2221e44f07e0191829223104593aa62af
DOK-APBY3 <arbuz2006@bk.ru>
2025-09-10 16:53:41 +0700

    added triangle class and sth in it

---
+++ geometry drower/Triangle.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace geometry_drower
+{
+    class Triangle
+    {
+        private PointClass point1, point2, point3;
+
+        public Triangle(PointClass point1, PointClass point2, PointClass point3)
+        {
+            this.point1 = point1;
+            this.point2 = point2;
+            this.point3 = point3;
+        }
+
+        public void move(int DelX, int DelY)
+        {
+            movePoint(point1, DelX, DelY);
+            movePoint(point2, DelX, DelY);
+            movePoint(point3, DelX, DelY);
+        }
+
+        private void movePoint(PointClass point, int Dx, int Dy)
+        {
+            point.addXP(Dx);
+            point.addYP(Dy);
+        }
+
+    }
+}
