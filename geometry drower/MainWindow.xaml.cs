edb5f8131f6632799807f7078e2b84c274e2e881
DOK-APBY3 <arbuz2006@bk.ru>
2025-09-17 16:54:20 +0700

    added moove handler

--- geometry drower/MainWindow.xaml.cs
+++ geometry drower/MainWindow.xaml.cs
@@ -16,7 +16,7 @@ public partial class MainWindow : Window
 {
 
     Triangle triang;
-    Square squ, reect;
+    Square squ, rect;
 
     Random rnd = new Random();
     public MainWindow()
@@ -74,29 +74,44 @@ public partial class MainWindow : Window
         PointClass point3 = new PointClass(rnd.Next(0, (int)Scene.Width), rnd.Next(0, (int)Scene.Height));
 
         triang = new Triangle(point1, point2, point3);
+        triang.MooveEvent += moover;
 
        
     }
 
     public void createSqare() // квадрат - дандомим длину ребра и сторим все точки строго от первой
     {
-            PointClass point1 = new PointClass(rnd.Next(0, (int)Scene.Width), rnd.Next(0, (int)Scene.Height)); // первая точка (девая верхняя)
-            int linelenght = (rnd.Next(1, ((int)Scene.Height) - Convert.ToInt32(point1.getX)));
-            PointClass point2 = new PointClass(Convert.ToInt32(point1.getX), Convert.ToInt32(point1.getY) + linelenght); // левая нижняя точка
-            PointClass point3 = new PointClass(Convert.ToInt32(point1.getX) + linelenght, Convert.ToInt32(point1.getY)); // правая верхняя
-            PointClass point4 = new PointClass(Convert.ToInt32(point1.getX) + linelenght, Convert.ToInt32(point1.getY) + linelenght); // спросить почему ругаеттся еси убрать конверт правая нижняя
+        PointClass point1 = new PointClass(rnd.Next(0, (int)Scene.Width), rnd.Next(0, (int)Scene.Height)); // первая точка (девая верхняя)
+        int linelenght = (rnd.Next(1, ((int)Scene.Height) - Convert.ToInt32(point1.getX)));
+        PointClass point2 = new PointClass(Convert.ToInt32(point1.getX), Convert.ToInt32(point1.getY) + linelenght); // левая нижняя точка
+        PointClass point3 = new PointClass(Convert.ToInt32(point1.getX) + linelenght, Convert.ToInt32(point1.getY)); // правая верхняя
+        PointClass point4 = new PointClass(Convert.ToInt32(point1.getX) + linelenght, Convert.ToInt32(point1.getY) + linelenght); // спросить почему ругаеттся еси убрать конверт правая нижняя
+        squ = new Square(point1, point2, point3, point4);
+        squ.MooveEvent += moover;
     }
 
     public void createRectangle() // прямоугольник - рандомим длину и ширину, принцип похож на квадрат
     {
-            PointClass point1 = new PointClass(rnd.Next(0, (int)Scene.Width), rnd.Next(0, (int)Scene.Height)); // первая точка (девая верхняя)
-            int xlenght = (rnd.Next(1, ((int)Scene.Width) - Convert.ToInt32(point1.getX)));
-            int yLenght = (rnd.Next(1, ((int)Scene.Height) - Convert.ToInt32(point1.getX)));
-            PointClass point2 = new PointClass(Convert.ToInt32(point1.getX), Convert.ToInt32(point1.getY) + yLenght); // левая нижняя точка
-            PointClass point3 = new PointClass(Convert.ToInt32(point1.getX) + xlenght, Convert.ToInt32(point1.getY)); // правая верхняя
-            PointClass point4 = new PointClass(Convert.ToInt32(point1.getX) + xlenght, Convert.ToInt32(point1.getY) + yLenght); //  правая нижняя
+        PointClass point1 = new PointClass(rnd.Next(0, (int)Scene.Width), rnd.Next(0, (int)Scene.Height)); // первая точка (девая верхняя)
+        int xlenght = (rnd.Next(1, ((int)Scene.Width) - Convert.ToInt32(point1.getX)));
+        int yLenght = (rnd.Next(1, ((int)Scene.Height) - Convert.ToInt32(point1.getX)));
+        PointClass point2 = new PointClass(Convert.ToInt32(point1.getX), Convert.ToInt32(point1.getY) + yLenght); // левая нижняя точка
+        PointClass point3 = new PointClass(Convert.ToInt32(point1.getX) + xlenght, Convert.ToInt32(point1.getY)); // правая верхняя
+        PointClass point4 = new PointClass(Convert.ToInt32(point1.getX) + xlenght, Convert.ToInt32(point1.getY) + yLenght); //  правая нижняя
+        rect = new Square(point1, point2, point3, point4);
+        squ.MooveEvent += moover;
     }
 
+    void moover(string type)
+    {
+        if (type == "T")
+        {
 
+        }
+        else if (type == "S")
+        {
+
+        }
+    }
   
 }
